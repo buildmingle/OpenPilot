f148ab7d881db49fa649e0ce668f6ccbb39cfe44
allexanderbergmans <allexander.bergmans@student.elisa.be>
2026-06-09 12:02:28 +0000

    * Levels Check

---
+++ src/ci_electric/check.cpp
@@ -0,0 +1,16 @@
+/*
+ * Open-source auto-pilot software for robotics and vehicles.
+ * Control with precision, build with confidence.
+ *
+ * Copyright (C) 2026 WeVuild, Be
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+// #include "../../include/ci_electric/check.hpp"
